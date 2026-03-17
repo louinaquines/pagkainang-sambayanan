64884d83d5b18a4eccb418bacf35a726ae98b571
louinaquines <louinaquines@gmail.com>
2026-03-18 03:54:51 +0800

    add: import from googleauthcontrol

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class GoogleAuthController extends Controller
 {
