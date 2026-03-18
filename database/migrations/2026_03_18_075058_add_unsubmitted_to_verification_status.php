d717d733a348f9cc07fd073d7833e383e44cba69
louinaquines <louinaquines@gmail.com>
2026-03-18 16:05:13 +0800

    add unsubmitted to verification_status enum

--- database/migrations/2026_03_18_075058_add_unsubmitted_to_verification_status.php
+++ database/migrations/2026_03_18_075058_add_unsubmitted_to_verification_status.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -12,9 +12,6 @@ return new class extends Migration
     public function up(): void
     {   
         DB::statement("ALTER TABLE users MODIFY COLUMN verification_status ENUM('pending', 'approved', 'rejected', 'unsubmitted') DEFAULT 'unsubmitted'");
-        Schema::table('verification_status', function (Blueprint $table) {
-            //
-        });
     }
 
     /**
@@ -22,9 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-         DB::statement("ALTER TABLE users MODIFY COLUMN verification_status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending'");
-        Schema::table('verification_status', function (Blueprint $table) {
-            //
-        });
+        DB::statement("ALTER TABLE users MODIFY COLUMN verification_status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending'");
     }
 };
